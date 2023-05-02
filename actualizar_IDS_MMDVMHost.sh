035c3c9134b627360c901f23c950782ad8bef2a4
Manel <ea3eiz@ea3eiz.com>
2023-05-02 14:14:40 +0200

    Update actualizar_IDS_MMDVMHost.sh

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -11,7 +11,7 @@ NEGRO="\33[0;30m"
 clear
 echo ""
 echo -n "${AMARILLO}"
-echo "    **-ACTUALIZANDO IDS INDICATIVOS-EA3EI**"
+echo "    **-ACTUALIZANDO IDS INDICATIVOS-EA3EIEA4AOJ"
 echo -n "${VERDE}"
 #echo "ACTUALIZANDO DMRIds.dat para DVSWITCH"
 sleep 13
