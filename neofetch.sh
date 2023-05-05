a1d5eff58a8753f7d514431798d6bba65def13ce
Manel <ea3eiz@ea3eiz.com>
2023-05-06 00:03:10 +0200

    Update neofetch.sh

--- neofetch.sh
+++ neofetch.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
+NEGRO="\33[0;30m"
+
 neofetch
-echo "esta ventana se cerrará en 2o segundos"
+echo -n "${AMARILLO}"
+echo "ESTA VENTANA SE CERRARÁ EN 20 SEGUNDOS"
 sleep 20
