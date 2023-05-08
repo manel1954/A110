49664907c98fbcb1b63efa227907cdf3461640e3
Manel <ea3eiz@ea3eiz.com>
2023-05-08 18:05:11 +0200

    Update ejecutar_dump1090.sh

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-cd /home/pi/Desktop
-sudo cp Abrir_BlueDV.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dump1090.sh'" /home/pi/Abrir_BlueDV.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AVION_ON.png" /home/pi/Abrir_BlueDV.desktop
-sed -i "10c Name[es_ES]=Cerrar Dump1090" /home/pi/Abrir_BlueDV.desktop
-
-cd /home/pi
-sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
-sudo rm /home/pi/Abrir_BlueDV.desktop
-#cd /home/pi/dump1090   
+#SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+#cd /home/pi/Desktop
+#sudo cp Abrir_BlueDV.desktop /home/pi
+#sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_dump1090.sh'" /home/pi/Abrir_BlueDV.desktop
+#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AVION_ON.png" /home/pi/Abrir_BlueDV.desktop
+#sed -i "10c Name[es_ES]=Cerrar Dump1090" /home/pi/Abrir_BlueDV.desktop
+#
+#cd /home/pi
+#sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
+#sudo rm /home/pi/Abrir_BlueDV.desktop
+##cd /home/pi/dump1090   
 
 stick=$(awk "NR==40" /home/pi/status.ini)
 raw=$(awk "NR==42" /home/pi/status.ini)
