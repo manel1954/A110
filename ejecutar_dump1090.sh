788e1f70802336f58bbc382dc73875ff6c866f66
Manel <ea3eiz@ea3eiz.com>
2023-05-08 18:08:24 +0200

    Update ejecutar_dump1090.sh

--- ejecutar_dump1090.sh
+++ ejecutar_dump1090.sh
@@ -41,13 +41,13 @@ fi
 
 
 
-cd /home/pi/Desktop
-sudo cp Abrir_BlueDV.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_dump1090.sh'" /home/pi/Abrir_BlueDV.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/pi/Abrir_BlueDV.desktop
-sed -i "10c Name[es_ES]=Abrir Dump1090" /home/pi/Abrir_BlueDV.desktop
-#sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
-cd /home/pi
-sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
-sleep 1
-sudo rm /home/pi/Abrir_BlueDV.desktop
+#cd /home/pi/Desktop
+#sudo cp Abrir_BlueDV.desktop /home/pi
+#sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sh ejecutar_dump1090.sh'" /home/pi/Abrir_BlueDV.desktop
+#sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AVION_OFF.png" /home/pi/Abrir_BlueDV.desktop
+#sed -i "10c Name[es_ES]=Abrir Dump1090" /home/pi/Abrir_BlueDV.desktop
+##sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
+#cd /home/pi
+#sudo cp Abrir_BlueDV.desktop /home/pi/Desktop
+#sleep 1
+#sudo rm /home/pi/Abrir_BlueDV.desktop
