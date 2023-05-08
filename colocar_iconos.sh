a45b5bc4de54351b3b13489b104d26d4552dd5d8
Manel <ea3eiz@ea3eiz.com>
2023-05-08 22:47:51 +0200

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -15,7 +15,12 @@
 # recupera los iconos que se han borrado y se han convertido en icono de un libro
 # pero no quita los semaforos que se queden encendidos por error al reiniciar
 cp /home/pi/A110/Desktop/*.* /home/pi/Desktop
-sudo chmod 777 -R /home/pi/Desktop
+#sudo chmod 777 -R /home/pi/Desktop
+
+
+# AÑADIDO PROVISIONALMENTE
+# sh clonando_carpeta_qt.sh
+
 
 
 cp /home/pi/icons.screen0-1904x1023.rc /home/pi/.config/xfce4/desktop
